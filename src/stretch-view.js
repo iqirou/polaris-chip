11004d2477b4e95e0f2105623f6eb3ab298cad29
iqirou <iqiroukun@gmail.com>
2024-03-10 21:59:15 -0400

    final alteration

--- src/stretch-view.js
+++ src/stretch-view.js
@@ -9,11 +9,11 @@ export class StretchView extends LitElement {
     constructor() {
         super();
         //defaults
-        this.status = 3;
         this.sticky = false;
         this.open = true;
+        this.closed = (localStorage.getItem("close") == "true"? true : false);
         this.datetime = "FEBRUARY 29, 2024 12:00 AM";
-        this.summary = "TEST CAMPUS ALERT";
+        this.summary = "PENN STATE NEWS";
         this.text = "Occaecat laboris incididunt ea labore quis in qui commodo velit cillum et commodo. Dolore consectetur eu eu reprehenderit anim fugiat in nostrud anim magna enim nisi. Mollit est incididunt sint aliqua duis. Deserunt ut velit deserunt fugiat eiusmod. Do incididunt laborum aliqua cupidatat adipisicing fugiat reprehenderit cillum id. Minim minim elit occaecat id velit fugiat ea. Aliqua excepteur ea excepteur cillum esse voluptate non elit laboris laboris esse est sunt incididunt ullamco.";
         this.linkText = "Learn More";
         this.link = "https://www.psu.edu/news";
@@ -21,23 +21,38 @@ export class StretchView extends LitElement {
 
     static get styles() {
         return css`
+        :host {
+            display: flex;
+        }
         :host([sticky]) {
             position: sticky;
             top: 0;
+        }
+        :host([closed]) {
+            background-color: var(--background-color);
+            display: flex;
+            height: 64px;
+        }
+        :host([status="1"]) {
+            --background-color: lightblue;
+        }
+        :host([status="2"]) {
+            --background-color: #ffd100;
+        }
+        :host([status="3"]) {
+            --background-color: #E51313;
         }
         .stretch-wrapper {
             display: inline-flex;
         }
         .date-wrapper {
-            display: flex;
-            margin: 0px 16px;
+            display: inline-grid;
         }
         .date {
             margin: auto;
             margin-left: 64px;
-            display: left;
             color: white;
-            font-size: 11pt;
+            font-size: 10pt;
         }
         .alert-message-wrapper {
             padding: 16px;
@@ -50,8 +65,8 @@ export class StretchView extends LitElement {
         }
         .icon {
             margin: auto;
-            width: 75px;
-            height: 75px;
+            width: 60px;
+            height: 60px;
             transform: skew(-20deg);
         }
         .alert-text {
@@ -75,6 +90,7 @@ export class StretchView extends LitElement {
             color: white;
             margin: 16px 0px 0px 32px;
             padding: 0px 36px 0px 0px;
+            cursor: pointer;
         }
         .close-button:active {
             border: 4px dotted black;
@@ -83,31 +99,60 @@ export class StretchView extends LitElement {
     }
 
     render() {
-        var statusColor;
-        var textColor;
-        
+        const text = this.closed ? "TEST CAMPUS ALERT" : this.text;
+        const datetime = this.closed ? "" : this.datetime;
+        const summary = this.closed ? "" : this.summary;
+        const statusColor = this.statusColor;
+        const textColor = this.textColor;
+
         if(this.status === 3) {
-            statusColor = "#900C0C";
-            textColor = "#E51313";
+            if(this.closed) {
+                this.statusColor = "#E51313";
+                this.textColor = "#E51313";
+            } else {
+                this.statusColor = "#900C0C";
+                this.textColor = "#E51313";
+            }
         }else if(this.status === 2) {
-            statusColor = "#bf8226";
-            textColor = "#ffd100";
+            if(this.closed) {
+                this.statusColor = "#ffd100";
+                this.textColor = "#ffd100";
+            } else {
+                this.statusColor = "#bf8226";
+                this.textColor = "#ffd100";
+            }
         }else if(this.status === 1){
-            statusColor = "darkblue";
-            textColor = "lightblue";
-        }
+            if(this.closed) {
+                this.statusColor = "lightblue";
+                this.textColor = "lightblue";
+            } else {
+                this.statusColor = "darkblue";
+                this.textColor = "lightblue";
+            }
+        }else{
+            if(this.closed) {
+                this.textColor = textColor;
+                this.statusColor = textColor;
+            } else {
+                this.statusColor = statusColor;
+                this.textColor = textColor;
+            }
+        } 
 
         return html`
-        <div class="stretch-wrapper" style="background-color: ${statusColor};">
+        <div class="stretch-wrapper" style="background-color: ${this.statusColor};">
             <div class="date-wrapper">
-                <p class="date"><b>${this.datetime}</b></p>
+                <p class="date"><b>${datetime}</b></p>
             </div>
-            <div class="alert-message-wrapper" style="background-color: ${textColor};">
-                <img class="icon" src="./src/images/mark.png">
-                <p class="alert-text"><b>${this.text} </b><a href="${this.link}" class="more-info"><b>PENN STATE NEWS</b></a></p>
+            <div class="alert-message-wrapper" style="background-color: ${this.textColor};">
+                <img id="ex" class="icon" src="./src/images/mark.png">
+                <p class="alert-text">
+                    <slot><b>${text} </b></slot>
+                    <a href="${this.link}" class="more-info"><b>${summary}</b></a>
+                </p>
             </div>
             <div class="button-wrapper">
-                <button class="close-button">
+                <button class="close-button" @click=${this.toggleButton}>
                     <b>X CLOSE</b>
                 </button>
             </div>
@@ -115,16 +160,39 @@ export class StretchView extends LitElement {
         `;
     }
 
+    updated(changedProperties) {
+        if (changedProperties.has("closed"))
+        {
+            if (this.closed){
+                localStorage.setItem("close", (this.closed).toString());
+            }
+            else{
+                localStorage.removeItem("close");
+            }
+        }
+    }
+  
+    toggleButton() {
+        if(this.closed){
+            this.closed = false;
+        } else if(!this.closed) {
+            this.closed = true;
+        }
+      }
+
     static get properties() {
         return{
             status: { type: Number , reflect: true },
             summary: { type: String },
             text: { type: String },
+            statusColor: { type: String },
+            textColor: { type: String },
             datetime: { type: String },
             linkText: { type: String },
             link: { type: String },
             sticky: { type: Boolean , reflect: true },
-            open: { type: Boolean , reflect: true }
+            open: { type: Boolean , reflect: true },
+            closed: { type: Boolean , reflect: true }
         };
     }
 }
